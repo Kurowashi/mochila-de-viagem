e6e5aa1d8b73f3a1cd59719e9147739f44ca044e
Luan Rocha <kuro.winchester@gmail.com>
2023-03-17 19:46:11 -0300

    Implementação localstorage

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    console.log(evento)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    criaElemento(nome.value, quantidade.value)
+    nome.value = ""
+    quantidade.value = ""
+
 })
 
 function criaElemento(nome, quantidade) {
@@ -18,5 +23,15 @@ function criaElemento(nome, quantidade) {
 
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += nome
+
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
